c21bedf9cf3c306fcab663a34955b32563dfdcf6
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 14:04:07 +0530

    Update Exp3_Multiplier_2bit.v

--- Exp3_Multiplier_2bit.v
+++ Exp3_Multiplier_2bit.v
@@ -1,12 +1,27 @@
-module ha(a,b,sum,carry);
+module ha(a,b,sum,c);
+
 input a,b;
-output sum,carry;
+
+output sum,c;
+
+xor g1(sum,a,b);
+
+and g2(c,a,b);
 
 endmodule
 
-module multi_2(a,b,p,carry);
+module bitmultiplier(a,b,c);
+
 input [1:0]a,b;
-output [2:0]p;
-output carry;
+
+output[3:0]c;
+
+wire w1;
+
+and g1(c[0],b[0],a[0]);
+
+ha ha1(a[0]&b[1],a[1]&b[0],c[1],w1);
+
+ha ha2(a[1] &b[1],w1,c[2],c[3]);
 
 endmodule
